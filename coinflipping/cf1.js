246f25522ccd5d1886d5750d96a62de8ba794dcf
DrDue <dr.due98@gmail.com>
2020-09-01 12:39:48 +0200

    coinflip

--- coinflipping/cf1.js
+++ coinflipping/cf1.js
@@ -1,79 +1,98 @@
 'use strict';
-
-let tails = 0;
 let heads = 0;
-let flip;
+let tails = 0;
 
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-flip = Math.random();
-flip = Math.floor(flip * 2 + 1);
-if (flip === 1)
-  tails++;
-else
-  heads++;
-
-document.write('heads: ' + heads + "<br>");
-document.write('tails: ' + tails + '<br>');
-document.write('yo waddup Niels, det er jo ez piz' + '<br>');
+function coinToss(numTimes) {
+  for (var i = 0; i < numTimes; i++) {
+    var coin = Math.floor(Math.random() * 10);
+    var even = (coin % 2 === 0);
+    if (even == true) {
+      heads++;
+    } else {
+      tails++;
+    }
+    console.log("The coin was " + (even ? "heads " : " tails"));
+    document.write("The coin was " + (even ? "heads " + "<br>" : " tails" + "<br>"))
+  }
+  document.write("<br>" + "total of heads: " + heads + "<br>");
+  document.write("total of tails " + tails);
+}
+// Now, call the function and tell it how many times to loop
+coinToss(10);
+// let tails = 0;
+// let heads = 0;
+// let flip;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// flip = Math.random();
+// flip = Math.floor(flip * 2 + 1);
+// if (flip === 1)
+//   tails++;
+// else
+//   heads++;
+//
+// document.write('heads: ' + heads + "<br>");
+// document.write('tails: ' + tails + '<br>');
+// document.write('yo waddup Niels, det er jo ez piz' + '<br>');
