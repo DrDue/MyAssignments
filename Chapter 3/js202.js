f2b2664b64e27e2412102e13ba9ec45732397a81
DrDue <dr.due98@gmail.com>
2020-09-03 09:01:41 +0200

    cpr

--- Chapter 3/js202.js
+++ Chapter 3/js202.js
@@ -1,4 +1,5 @@
 var cpr = prompt("Cpr")
 var kvinde = (cpr % 2 == 0);
-console.log(koen);
+console.log(kvinde);
 document.write("Er det en kvinde? " + kvinde);
+console.log(`Er det en kvinde? ${99999999 < cpr && cpr < 9999999999 && cpr % 2 === 0}`);
